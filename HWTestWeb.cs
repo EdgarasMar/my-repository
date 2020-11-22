24f0c5d623501d929f52d97df74064549fcdaed4
EdgarasMar <Edgaras.mar@gmail.com>
2020-11-22 20:55:32 +0200

    my first trial

---
+++ HWTestWeb.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgramAuto
+{
+    class HWTestWeb
+    {
+        [Test]
+
+        public static void Givin_PositiveNumber_When_WebCalkulator_Return_Result()
+        {
+            // Arange
+
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Url = "http://www.seleniumeasy.com/test/basic-first-form-demo.html";
+
+            //Act
+
+            IWebElement inputField1 = chrome.FindElement(By.Id("sum1"));
+            string myText = "2";
+            inputField1.SendKeys(myText);
+            IWebElement inputField2 = chrome.FindElement(By.Id("sum2"));
+            string myText2 = "2";
+            inputField2.SendKeys(myText2);
+            IWebElement button = chrome.FindElement(By.CssSelector("#gettotal > button"));
+            button.Click();
+            IWebElement result = chrome.FindElement(By.Id("displayvalue"));
+
+            //Assert
+            Assert.AreEqual("4", result.Text, "Test is wrong");
+            chrome.Quit();
+        }
+
+        [Test]
+
+        public static void Givin_NegativeNumber_When_WebCalkulator_Return_Result()
+        {
+            // Arange
+
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Url = "http://www.seleniumeasy.com/test/basic-first-form-demo.html";
+
+            //Act
+
+            IWebElement inputField1 = chrome.FindElement(By.Id("sum1"));
+            string myText = "-5";
+            inputField1.SendKeys(myText);
+            IWebElement inputField2 = chrome.FindElement(By.Id("sum2"));
+            string myText2 = "3";
+            inputField2.SendKeys(myText2);
+            IWebElement button = chrome.FindElement(By.CssSelector("#gettotal > button"));
+            button.Click();
+            IWebElement result = chrome.FindElement(By.Id("displayvalue"));
+
+            //Assert
+
+            Assert.AreEqual("-2", result.Text, "Wrong test");
+            chrome.Quit();
+        }
+
+        [Test]
+
+        public static void Givin_NotNumber_When_WebCalkulator_Return_NaNString()
+        {
+            // Arange
+
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Url = "http://www.seleniumeasy.com/test/basic-first-form-demo.html";
+
+            //Act
+
+            IWebElement inputField1 = chrome.FindElement(By.Id("sum1"));
+            string myText = "a";
+            inputField1.SendKeys(myText);
+            IWebElement inputField2 = chrome.FindElement(By.Id("sum2"));
+            string myText2 = "b";
+            inputField2.SendKeys(myText2);
+            IWebElement button = chrome.FindElement(By.CssSelector("#gettotal > button"));
+            button.Click();
+            IWebElement result = chrome.FindElement(By.Id("displayvalue"));
+
+            //Assert
+
+            Assert.AreEqual("NaN", result.Text, "Test not working" );
+            chrome.Quit();
+        }
+    }
+}
---
+++ TestWebDriver.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgramAuto
+{
+    class TestWebDriver
+    {
+
+        [Test]
+        public static void TestChromeDriver()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Url = "https://login.yahoo.com/";
+            chrome.Quit();
+
+        }
+        [Test]
+        public static void TestFirefoxDriver()
+        {
+            IWebDriver firefox = new FirefoxDriver();
+            firefox.Url = "https://login.yahoo.com/";
+            firefox.Quit();
+        }
+        [Test]
+
+
+        public static void TestYahooPage()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Url = "https://login.yahoo.com/";
+            IWebElement loginInputField = chrome.FindElement(By.Id("login-username"));
+            loginInputField.SendKeys("Test");
+            IWebElement loginButton = chrome.FindElement(By.Id("login-signin"));
+            loginButton.Click();
+            // chrome.Quit();
+        }
+
+        [Test]
+
+        public static void TestSeleniumPage()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Url = "http://www.seleniumeasy.com/test/basic-first-form-demo.html";
+            IWebElement inputField = chrome.FindElement(By.Id("user-message"));
+            string myText = "Cat";
+            inputField.SendKeys(myText);
+            //IWebElement buttonNop = chrome.FindElement(By.CssSelector(".btn.btn-default"));
+            IWebElement button = chrome.FindElement(By.CssSelector("#get-input > button"));
+            //IWebElement buttonNoppp = chrome.FindElement(By.XPath("//[@id='get-input']/button"));
+            button.Click();
+            IWebElement result = chrome.FindElement(By.Id("display"));
+            //Assert.IsTrue(myText.Equals(result), "No Text");
+            Assert.AreEqual(myText, result.Text, "Testas neatsirado");
+            chrome.Quit();
+        }
+
+        [Test]
+        public static void TestSeleniumPageFirefox()
+        {
+            IWebDriver firefox = new FirefoxDriver();
+            firefox.Url = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
+            IWebElement inputField = firefox.FindElement(By.Id("user-message"));
+            string myText = "Katinukai";
+            inputField.SendKeys(myText);
+            IWebElement button = firefox.FindElement(By.CssSelector("#get-input > button"));
+            button.Click();
+            IWebElement result = firefox.FindElement(By.Id("display"));
+            //Assert.IsTrue(myText.Equals(result), "Tekstas neatsirado");
+            Assert.AreEqual(myText, result.Text, "Tekstas neatsirado");
+            firefox.Quit();
+
+        }
+    }
+}
